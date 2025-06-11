7ceeaa274a99d5720f206b51a8c9c007a68feb58
Irvin Baez <baez.irv@gmail.com>
2025-06-10 19:13:53 -0600

    Initial commit

---
+++ BlackJackLight/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace BlackJackLight
+{
+	class Program
+	{
+		static void Main (string[] args)
+		{
+			Console.Write("BlackJackLight v1.0.0\n");
+			Console.WriteLine("Hello World!");
+		}
+	}
+}
